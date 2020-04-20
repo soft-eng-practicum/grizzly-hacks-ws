c96927b90ec915a0e7e98028a50a20d2a7b10313
unknown <josiahcwills@gmail.com>
2020-04-20 09:48:27 -0400

    Changed color of border, will talk about later today

--- reactproto/src/footer.js
+++ reactproto/src/footer.js
@@ -13,7 +13,7 @@ import Fade from 'react-reveal/Fade';
  * This element constructs the footer of all the web pages
  * The icons will link to the Grizzly Hacks social media accounts and git hub.
  * Header and Icon are parented by <Fade> so that the gradually appear on the page as the user scrolls down.
- * Developer can edit the link by changing the href for the icon.
+ * Developer can edit the link by changing the href in <a> for the icon.
  */
 const pageFooter= () => (
   <Segment id="footerContent" inverted>
@@ -25,7 +25,7 @@ const pageFooter= () => (
                <Fade>
                 <Header inverted as='h4' content='Contact Us' id="contact"/>
                   <div>
-                    {/* Has icons for the contact info in the footer */}
+                    {/* Has icons for the contact info in the footer. Links to the Grizzly Hacks social media */}
                     <a target="_blank" href="https://discord.gg/m8rKscc"><Icon link name ='discord' size='large' /></a>
                     <a target="_blank" href="https://github.com/soft-eng-practicum/grizzly-hacks-ws"><Icon link name='github' size='large'/></a>
                     <a target="_blank" href="https://www.instagram.com/"><Icon link name='instagram' size='large' /></a>
